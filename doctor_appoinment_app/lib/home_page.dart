19eefbe93b298d6bcf5efe3ceecb5e09177bda72
iqbalriiaz <iqbalriiaz@gmail.com>
2022-10-30 23:08:12 +0600

    riaz

---
+++ doctor_appoinment_app/lib/home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomaPage extends StatefulWidget {
+  const HomaPage({super.key});
+
+  @override
+  State<HomaPage> createState() => _HomaPageState();
+}
+
+class _HomaPageState extends State<HomaPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
